769c0447d6fc17c49fe992ee6cd3aaa98c62104d
Catalina Carreto <iamkcoder@gmail.com>
2022-01-11 17:29:03 -0600

    chore: Configure express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hola mundo');
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
+//Crear el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+
+    console.log('se requiere /');
+    res.json({
+        ok: true
+    })
+});
+
+
+//Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
