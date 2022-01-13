7cb24d326bb613cf7b644b04b72b6849b75ee806
Catalina Carreto <iamkcoder@gmail.com>
2022-01-12 18:59:44 -0600

    feat: Create user un database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,27 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async(req, res = response) => {
 
-    const {name, email, password } = req.body;
+    //const {name, email, password } = req.body;
+    
+    try {
+        const usuario = new Usuario(req.body);
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email, 
-        password
-    })
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'por favor hable con el administrador'
+        });
+    }
 }
 const loginUsuario =  (req, res = response) => {
 
