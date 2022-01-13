08a8b2daaa67a1935a1398cfa9d8f6dd2e0852c1
Catalina Carreto <iamkcoder@gmail.com>
2022-01-13 12:58:19 -0600

    feat: Generate JSON WEB TOKEN

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async(req, res = response) => {
 
@@ -24,10 +25,14 @@ const crearUsuario = async(req, res = response) => {
 
         await usuario.save();
 
+        //Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name);
+
         res.status(201).json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         });
 
     } catch (error) {
@@ -62,11 +67,13 @@ const loginUsuario =  async(req, res = response) => {
             });
         }
         //Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name);
 
         res.json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         });
 
     } catch (error) {
