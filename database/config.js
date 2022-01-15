a2f4a1294f25ab6f924ad62fd1ad61ce437cc174
Catalina Carreto <iamkcoder@gmail.com>
2022-01-14 19:27:05 -0600

    refactor: Modify connection to dababase

--- database/config.js
+++ database/config.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
 
         console.log('DB Online');
