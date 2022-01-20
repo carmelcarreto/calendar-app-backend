c0ded7d10c3fdb0858b243b4bd389a723ca9ec8b
Catalina Carreto <iamkcoder@gmail.com>
2022-01-19 19:04:25 -0600

    feat: Create version 1 of the Calendar App

---
+++ public/precache-manifest.332625367880a51c2f3009d81adb0557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ebe5162ebb3329b136",
+    "url": "/static/css/main.5fe5447e.chunk.css"
+  },
+  {
+    "revision": "48ebe5162ebb3329b136",
+    "url": "/static/js/main.48ebe516.chunk.js"
+  },
+  {
+    "revision": "da85072bbafd64f291ce",
+    "url": "/static/css/1.4cc153ce.chunk.css"
+  },
+  {
+    "revision": "da85072bbafd64f291ce",
+    "url": "/static/js/1.da85072b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80ff416281656611048eaad9e140471f",
+    "url": "/index.html"
+  }
+];
